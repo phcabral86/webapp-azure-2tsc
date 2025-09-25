56db4a1d4609e8ec990f0f0fce6142660af04b61
rksakai <95721924+rksakai@users.noreply.github.com>
2025-09-25 18:47:50 -0300

    Ajuste

--- webapp.tf
+++ webapp.tf
@@ -28,19 +28,11 @@ resource "azurerm_linux_web_app" "webapp" {
     DB_NAME                       = azurerm_mysql_flexible_database.mysql_database.name
     DB_USER                       = azurerm_key_vault_secret.my_password.value
     DB_PORT                       = "3306"
-    SECRET_KEY                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.vault.vault_uri}secrets/${azurerm_key_vault_secret.my_password.name}/)"
+    SECRET_KEY                    = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.vault.name};SecretName=${azurerm_key_vault_secret.my_password.name})"
     SCM_DO_BUILD_DURING_DEPLOYMENT = "true"  # No Terraform sempre como string
     MYSQL_CHARSET                 = "utf8mb4"
     MYSQL_COLLATION               = "utf8mb4_unicode_ci"
   }
-
-  # app_settings como argumento de nível superior
-#  app_settings = {
-#    "MYHOST"     = azurerm_mysql_flexible_server.mysql.fqdn
-#    "MYUSER"     = var.mysql_admin
-    #"PGPASSWORD" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.my_password.id})"
-#    "MYSQL_DB" = azurerm_mysql_flexible_database.mysql_database.name
-#  }
 
   # site_config separado
   site_config {
@@ -49,25 +41,3 @@ resource "azurerm_linux_web_app" "webapp" {
     }
   }
 }
-
-# Recurso Service Connector para PostgreSQL
-#resource "azurerm_app_service_connection" "webapp_postgres" {
-#  name           = "webapp_to_postgresql_connector"
-#
-  # ID da Web App (fonte)
-#  app_service_id = azurerm_linux_web_app.webapp.id
-
-  # ID do MySQL (destino)
-#  target_resource_id = azurerm_mysql_flexible_server.mysql.id
-
-  # Configuração de autenticação (exemplo com usuário/senha)
-#  authentication {
-#    type = "systemAssignedIdentity"  # Tipo de autenticação
-#  }
-
-#  depends_on = [
-#    azurerm_linux_web_app.webapp,
-#    azurerm_mysql_flexible_server.mysql
-#    
-#  ]
-#}
